196c35f342becf81acd4c39ce3194815c743d153
Matchu Pitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-04-12 18:27:59 +0200

    First try merge pokemonImg into our API

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -2,11 +2,11 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faAngleLeft,
   faAngleRight,
-  faHeart,
+  faHeart
 } from "@fortawesome/free-solid-svg-icons";
 
 import {} from "@fortawesome/free-regular-svg-icons";
 
 import {} from "@fortawesome/free-brands-svg-icons";
 
-library.add(faAngleLeft, faAngleRight);
+library.add(faAngleLeft, faAngleRight, faHeart);
