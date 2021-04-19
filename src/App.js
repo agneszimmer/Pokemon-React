9e54c092dde3bd06001615ae95821bc1cab82a1b
Matchu Pitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-04-19 09:28:59 +0200

    Update style searchbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import AllPokemon from "./components/AllPokemon";
 import PokeFight from "./components/PokeFight";
 import Stats from "./components/Stats";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
